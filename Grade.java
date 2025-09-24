9e2f9cdd0b4b56c92678fc415c37549ed7b10d46
AGGOUN <houdaaggoun3@gmail.com>
2025-09-24 03:01:31 +0100

    modif 2 grade.java

--- Grade.java
+++ Grade.java
@@ -3,7 +3,7 @@ void assignGrade(String studentName, String course, double grade){
 
 }
 void displayGrades(){
-    system.out.println("hello grade");
+    system.out.println("hello Houda");
     
 }
 
