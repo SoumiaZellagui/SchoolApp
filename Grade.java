00a78f7ad14c8a1488582dc16990bd40444f6d03
AGGOUN <houdaaggoun3@gmail.com>
2025-09-24 03:15:04 +0100

    Revert "modif 2 grade.java"
    This reverts commit 9e2f9cdd0b4b56c92678fc415c37549ed7b10d46.

--- Grade.java
+++ Grade.java
@@ -3,7 +3,7 @@ void assignGrade(String studentName, String course, double grade){
 
 }
 void displayGrades(){
-    system.out.println("hello Houda");
+    system.out.println("hello grade");
     
 }
 
