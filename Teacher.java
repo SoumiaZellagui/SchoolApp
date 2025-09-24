26611696bd59378a95fd53e7208f01a9e11a93fd
radiaboudjemil <radia7589@gmail.com>
2025-09-24 12:43:01 +0200

    ADD Methode displayTeacher

--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,10 @@
 public class Teacher{
 
-
+// Method to display teacher details
+    public void displayTeacher() {
+        System.out.println("Teacher Name: " + name);
+        System.out.println("Subject: " + subject);
+    }
 
 
     
