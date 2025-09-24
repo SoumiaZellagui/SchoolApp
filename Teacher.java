f3b42c9cacc25ee8fdd361b7b41475b21be2c0fd
souhila1991 <sou.dsi0608@gmail.com>
2025-09-24 11:42:12 +0100

    Add Method AddTeacher

--- Teacher.java
+++ Teacher.java
@@ -1,7 +1,15 @@
 public class Teacher{
+    private String name;
+    private String subject;
 
+    public Teacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
 
-
-
+    public void addTeacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
     
 }
